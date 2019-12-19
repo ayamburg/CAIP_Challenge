f8bfff743bb542c93fca77018fd62d35c801c486
Andrey Yamburg <andreyamburg@gmail.com>
2019-12-18 23:32:51 -0800

    Remove key

--- app/interactors/youtube_interactor.rb
+++ app/interactors/youtube_interactor.rb
@@ -1,6 +1,6 @@
 class YoutubeInteractor
 
-  KEY = 'AIzaSyD2pJdbsV7JZUzE9ubq8W9l5M8MHumwFjM'
+  KEY = ''
   BASE_URI = 'https://www.googleapis.com/youtube/v3/'.freeze
 
   def initialize(query = '', ids = [])
