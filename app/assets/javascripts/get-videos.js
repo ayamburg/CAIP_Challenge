47e5cdafa55a502a5f52f7620271910823b71d32
ayamburg <ayamburg@ucsc.edu>
2019-12-22 04:29:53 -0800

    Add accordion panel content

--- app/assets/javascripts/get-videos.js
+++ app/assets/javascripts/get-videos.js
@@ -32,10 +32,7 @@ function buildVideoAccordion(videos){
         var published = $("<td>");
         published.addClass("date");
         var publishedAt = new Date(item['snippet']['publishedAt']);
-        console.log(publishedAt);
         publishedAt = publishedAt.toLocaleString("en-US",  { year: 'numeric', month: 'long', day: 'numeric' });
-        console.log(publishedAt);
-        console.log(publishedAt.toString());
         published.text(publishedAt);
         row.append(title);
         row.append(channel);
@@ -46,10 +43,53 @@ function buildVideoAccordion(videos){
         //Build accordion foldout
         var panel = $("<div>");
         panel.addClass('panel');
-        var panel = $("<div>");
-        var panelText = $("<p>");
-        panelText.text(item['snippet']['description']);
-        panel.append(panelText);
+        var panelTop = $("<div>");
+        panelTop.addClass('panel-top');
+        var thumbnail = $("<img>");
+        thumbnail.attr('src', item['snippet']['thumbnails']['standard']['url']);
+        var panelText = $("<div>");
+        panelText.addClass('panel-text');
+        var descriptionLabel = $("<p>");
+        descriptionLabel.addClass('label');
+        descriptionLabel.text('DESCRIPTION');
+        var description = $("<p>");
+        description.text(item['snippet']['description']);
+        panelText.append(descriptionLabel);
+        panelText.append(description);
+        panelTop.append(thumbnail);
+        panelTop.append(panelText);
+        panel.append(panelTop);
+
+        var statsTable = $("<table>");
+        var statsLabelsRow = $("<tr>");
+        var viewsLabel = $("<th>");
+        viewsLabel.addClass('label');
+        viewsLabel.text("VIEW COUNT");
+        var likesLabel = $("<th>");
+        likesLabel.addClass('label');
+        likesLabel.text("LIKE COUNT");
+        var tagsLabel = $("<th>");
+        tagsLabel.addClass('label');
+        tagsLabel.text("TAGS");
+        statsLabelsRow.append(viewsLabel);
+        statsLabelsRow.append(likesLabel);
+        statsLabelsRow.append(tagsLabel);
+        statsTable.append(statsLabelsRow);
+        var statsRow = $("<tr>");
+        var views = $("<td>");
+        views.text(item['statistics']['viewCount']);
+        var likes = $("<td>");
+        likes.text(item['statistics']['likeCount']);
+        var tags = $("<td>");
+        var tagList = item['snippet']['tags'];
+        if(tagList) {
+            tags.text(tagList.join(', '));
+        }
+        statsRow.append(views);
+        statsRow.append(likes);
+        statsRow.append(tags);
+        statsTable.append(statsRow);
+        panel.append(statsTable);
 
         videosAccordion.append(button);
         videosAccordion.append(panel);
